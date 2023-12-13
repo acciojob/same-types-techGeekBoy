b8de4f5c74f48e4192499b964de1e19af128a641
techGeekBoy <148032840+techGeekBoy@users.noreply.github.com>
2023-12-13 10:33:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-function isSameType(value1, value2)
-	const value1="hello";
-    const value2="world";
-	{
-	if(isSameType(NaN NaN)){
-	 if (isNaN(value1) && isNaN(value2)) {
+import test from 'ava';
+
+function isSameType(value1, value2) {
+    if (isNaN(value1) && isNaN(value2)) {
         return true; // Both values are NaN
     }
-    
+
     return typeof value1 === typeof value2;
-	}
-  //your js code here
 }
 
-// do not change the code below.
- let value1 = prompt("Enter Start of the Range.");
- let value2 = prompt("Enter End Of the Range.");
-alert(isSameType(value1, value2));
+test('isSameType - numbers of the same type', t => {
+    t.true(isSameType(1, 3));
+});
+
+test('isSameType - strings of the same type', t => {
+    t.true(isSameType("hey", "hello"));
+});
+
+test('isSameType - NaN values', t => {
+    t.true(isSameType(NaN, NaN));
+});
+
+test('isSameType - values of different types', t => {
+    t.false(isSameType("3", 3));
+});
