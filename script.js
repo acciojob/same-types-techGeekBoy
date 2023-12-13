83cd3a1a69761155066e64b35577d3738af9e1d6
techGeekBoy <148032840+techGeekBoy@users.noreply.github.com>
2023-12-13 10:34:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-import test from 'ava';
-
 function isSameType(value1, value2) {
     if (isNaN(value1) && isNaN(value2)) {
         return true; // Both values are NaN
     }
-
+    
     return typeof value1 === typeof value2;
 }
 
-test('isSameType - numbers of the same type', t => {
-    t.true(isSameType(1, 3));
-});
-
-test('isSameType - strings of the same type', t => {
-    t.true(isSameType("hey", "hello"));
-});
-
-test('isSameType - NaN values', t => {
-    t.true(isSameType(NaN, NaN));
-});
-
-test('isSameType - values of different types', t => {
-    t.false(isSameType("3", 3));
-});
+// Examples
+console.log(isSameType(1, 3)); // true
+console.log(isSameType("hey", "hello")); // true
+console.log(isSameType(NaN, NaN)); // true
+console.log(isSameType("3", 3)); // false
